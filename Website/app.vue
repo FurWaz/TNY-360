e134dfc43ed83abf9d712f5e4ac5f3c308e62d46
FurWaz <fur.waz06@gmail.com>
2024-10-24 09:34:06 +0200

    Added website base files

---
+++ Website/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex min-w-screen min-h-screen bg-zinc-100 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-100">
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </div>
+</template>
+
+<script lang="ts">
+import '~/assets/style.css';
+</script>
---
+++ Website/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+    <div class="min-h-screen w-full">
+        <div class="flex flex-col space-y-2 justify-center items-center w-full h-screen">
+            <h1 class="text-7xl font-main">TNY - 360</h1>
+            <p class="text-xl">Un petit robot-araignée quadrupède, rempli de capteurs !</p>
+        </div>
+    </div>
+</template>
