8ecf27ee758429311f9efffcdea4302f3abbbd1b
FurWaz <fur.waz06@gmail.com>
2024-10-24 09:50:26 +0200

    Fixed website build problems

--- Website/app.vue
+++ Website/app.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex min-w-screen min-h-screen bg-zinc-100 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-100">
-    <NuxtRouteAnnouncer />
-    <NuxtPage />
-  </div>
+    <div>
+        <NuxtRouteAnnouncer />
+    </div>
+    <div class="flex min-w-screen min-h-screen bg-zinc-100 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-100">
+        <NuxtPage />
+    </div>
 </template>
 
-<script lang="ts">
+<script>
 import '~/assets/style.css';
 </script>
